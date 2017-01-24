59a5994d1cd304206a3f715254910a61c081b5f3
oONeVeNOo <koliushko.a@gmail.com>
2017-01-25 00:25:40 +0200

    Project skeleton + Lombok

--- src/main/java/entities/Hotel.java
+++ src/main/java/entities/Hotel.java
@@ -1,6 +1,9 @@
 package entities;
 
-class Hotel extends Room {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter public class Hotel extends Room {
 
     private long id;
     private String name;
@@ -10,32 +13,6 @@ class Hotel extends Room {
         super(id, person, price, orderedUser);
         this.id = id1;
         this.name = name;
-        this.city = city;
-    }
-
-    @Override
-    public long getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
         this.city = city;
     }
 }
