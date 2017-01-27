276902e9ec2cc85a8f6cd84211b37ebe0c4126fc
oONeVeNOo <koliushko.a@gmail.com>
2017-01-27 20:19:58 +0200

    Constructor added

--- src/main/java/com/manardenza/utils/DatabaseSeeder.java
+++ src/main/java/com/manardenza/utils/DatabaseSeeder.java
@@ -19,6 +19,8 @@ public class DatabaseSeeder {
     private static final String HOLIDAY_INN = "Holiday Inn";
     private static final String REIKARTZ = "Reikartz";
 
+    private DatabaseSeeder() {
+    }
 
     @Getter private static List<User> usersList = new ArrayList<>();
     @Getter private static List<Room> roomsList = new ArrayList<>();
@@ -28,9 +30,9 @@ public class DatabaseSeeder {
         usersList.add(new User(464655, "Vasya", "Petrov"));
         usersList.add(new User(546466, "Anton", "Chechov"));
         usersList.add(new User(566656, "Natalia", "Novikova"));
-        usersList.add(new User(848466, "Igor", "Petrov"));
+        usersList.add(new User(848466, "Igor", "Sidorov"));
         usersList.add(new User(894656, "Vasya", "Ivanov"));
-        usersList.add(new User(894565, "Kolya", "Petrov"));
+        usersList.add(new User(894565, "Kolya", "Pyatkin"));
         usersList.add(new User(435168, "Andrey", "Semenov"));
         usersList.add(new User(684616, "Katya", "Zagorodnaja"));
         usersList.add(new User(684646, "Marina", "Semionova"));
