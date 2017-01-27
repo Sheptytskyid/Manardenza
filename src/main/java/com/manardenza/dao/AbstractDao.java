8e0f0f8f51de66572dacb342ed44e2c3d90e5381
oONeVeNOo <koliushko.a@gmail.com>
2017-01-27 22:09:03 +0200

    Connected DatabaseSeeder to AbstractDao

--- src/main/java/com/manardenza/dao/AbstractDao.java
+++ src/main/java/com/manardenza/dao/AbstractDao.java
@@ -3,14 +3,15 @@ package com.manardenza.dao;
 import com.manardenza.entity.AbstractObject;
 import com.manardenza.entity.Hotel;
 import com.manardenza.entity.User;
+import com.manardenza.utils.DatabaseSeeder;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class AbstractDao<T extends AbstractObject> {
+public abstract class AbstractDao<T extends AbstractObject> extends DatabaseSeeder {
 
-    protected List<Hotel> hotelList = new ArrayList<>();
-    protected List<User> userlList = new ArrayList<>();
+    protected List<Hotel> hotelList = hotelsSeederList;
+    protected List<User> userlList = usersSeederList;
 
     public T save(T obj) {
         return obj;
--- src/main/java/com/manardenza/utils/DatabaseSeeder.java
+++ src/main/java/com/manardenza/utils/DatabaseSeeder.java
@@ -19,52 +19,49 @@ public class DatabaseSeeder {
     private static final String HOLIDAY_INN = "Holiday Inn";
     private static final String REIKARTZ = "Reikartz";
 
-    private DatabaseSeeder() {
-    }
-
-    @Getter private static List<User> usersList = new ArrayList<>();
-    @Getter private static List<Room> roomsList = new ArrayList<>();
-    @Getter private static List<Hotel> hotelsList = new ArrayList<>();
+    @Getter protected static List<User> usersSeederList = new ArrayList<>();
+    @Getter protected static List<Room> roomsSeederList = new ArrayList<>();
+    @Getter protected static List<Hotel> hotelsSeederList = new ArrayList<>();
 
     static {
-        usersList.add(new User(464655, "Vasya", "Petrov"));
-        usersList.add(new User(546466, "Anton", "Chechov"));
-        usersList.add(new User(566656, "Natalia", "Novikova"));
-        usersList.add(new User(848466, "Igor", "Sidorov"));
-        usersList.add(new User(894656, "Vasya", "Ivanov"));
-        usersList.add(new User(894565, "Kolya", "Pyatkin"));
-        usersList.add(new User(435168, "Andrey", "Semenov"));
-        usersList.add(new User(684616, "Katya", "Zagorodnaja"));
-        usersList.add(new User(684646, "Marina", "Semionova"));
-        usersList.add(new User(584689, "Jozef", "Pavlik"));
-        usersList.add(new User(756161, "Dariusz", "Maciewski"));
-        usersList.add(new User(654846, "Andrzej", "Donica"));
-        usersList.add(new User(314688, "Sergey", "Godovany"));
-        usersList.add(new User(786146, "Scolastyka", "Machura"));
-        usersList.add(new User(894664, "Ann", "Merryvether"));
+        usersSeederList.add(new User(464655, "Vasya", "Petrov"));
+        usersSeederList.add(new User(546466, "Anton", "Chechov"));
+        usersSeederList.add(new User(566656, "Natalia", "Novikova"));
+        usersSeederList.add(new User(848466, "Igor", "Sidorov"));
+        usersSeederList.add(new User(894656, "Vasya", "Ivanov"));
+        usersSeederList.add(new User(894565, "Kolya", "Pyatkin"));
+        usersSeederList.add(new User(435168, "Andrey", "Semenov"));
+        usersSeederList.add(new User(684616, "Katya", "Zagorodnaja"));
+        usersSeederList.add(new User(684646, "Marina", "Semionova"));
+        usersSeederList.add(new User(584689, "Jozef", "Pavlik"));
+        usersSeederList.add(new User(756161, "Dariusz", "Maciewski"));
+        usersSeederList.add(new User(654846, "Andrzej", "Donica"));
+        usersSeederList.add(new User(314688, "Sergey", "Godovany"));
+        usersSeederList.add(new User(786146, "Scolastyka", "Machura"));
+        usersSeederList.add(new User(894664, "Ann", "Merryvether"));
 
-        roomsList.add(new Room("SINGLE ROOM", 1, 200, null));
-        roomsList.add(new Room("DOUBLE ROOM", 2, 300, null));
-        roomsList.add(new Room("KING BEDROOM", 2, 350, null));
-        roomsList.add(new Room("INTERCONNECTING ROOMS", 3, 400, null));
-        roomsList.add(new Room("INTERCONNECTING ROOMS", 4, 450, null));
-        roomsList.add(new Room("DUPLEX", 4, 700, null));
-        roomsList.add(new Room("SUITE ROOM", 6, 1000, null));
+        roomsSeederList.add(new Room("SINGLE ROOM", 1, 200, null));
+        roomsSeederList.add(new Room("DOUBLE ROOM", 2, 300, null));
+        roomsSeederList.add(new Room("KING BEDROOM", 2, 350, null));
+        roomsSeederList.add(new Room("INTERCONNECTING ROOMS", 3, 400, null));
+        roomsSeederList.add(new Room("INTERCONNECTING ROOMS", 4, 450, null));
+        roomsSeederList.add(new Room("DUPLEX", 4, 700, null));
+        roomsSeederList.add(new Room("SUITE ROOM", 6, 1000, null));
 
-        hotelsList.add(new Hotel(684464684, MARRIOTT, KIEV, roomsList));
-        hotelsList.add(new Hotel(649464689, MARRIOTT, ODESSA, roomsList));
-        hotelsList.add(new Hotel(849849464, MARRIOTT, LVOV, roomsList));
-        hotelsList.add(new Hotel(653548446, MARRIOTT, DONETSK, roomsList));
-        hotelsList.add(new Hotel(876616656, MARRIOTT, ZAPOROZHYE, roomsList));
-        hotelsList.add(new Hotel(214546684, HOLIDAY_INN, KIEV, roomsList));
-        hotelsList.add(new Hotel(756464564, HOLIDAY_INN, ODESSA, roomsList));
-        hotelsList.add(new Hotel(955545454, HOLIDAY_INN, LVOV, roomsList));
-        hotelsList.add(new Hotel(735465465, HOLIDAY_INN, DONETSK, roomsList));
-        hotelsList.add(new Hotel(315468787, HOLIDAY_INN, ZAPOROZHYE, roomsList));
-        hotelsList.add(new Hotel(346864847, REIKARTZ, KIEV, roomsList));
-        hotelsList.add(new Hotel(945425451, REIKARTZ, ODESSA, roomsList));
-        hotelsList.add(new Hotel(345684884, REIKARTZ, LVOV, roomsList));
-        hotelsList.add(new Hotel(753456466, REIKARTZ, DONETSK, roomsList));
-        hotelsList.add(new Hotel(456468498, REIKARTZ, ZAPOROZHYE, roomsList));
+        hotelsSeederList.add(new Hotel(684464684, MARRIOTT, KIEV, roomsSeederList));
+        hotelsSeederList.add(new Hotel(649464689, MARRIOTT, ODESSA, roomsSeederList));
+        hotelsSeederList.add(new Hotel(849849464, MARRIOTT, LVOV, roomsSeederList));
+        hotelsSeederList.add(new Hotel(653548446, MARRIOTT, DONETSK, roomsSeederList));
+        hotelsSeederList.add(new Hotel(876616656, MARRIOTT, ZAPOROZHYE, roomsSeederList));
+        hotelsSeederList.add(new Hotel(214546684, HOLIDAY_INN, KIEV, roomsSeederList));
+        hotelsSeederList.add(new Hotel(756464564, HOLIDAY_INN, ODESSA, roomsSeederList));
+        hotelsSeederList.add(new Hotel(955545454, HOLIDAY_INN, LVOV, roomsSeederList));
+        hotelsSeederList.add(new Hotel(735465465, HOLIDAY_INN, DONETSK, roomsSeederList));
+        hotelsSeederList.add(new Hotel(315468787, HOLIDAY_INN, ZAPOROZHYE, roomsSeederList));
+        hotelsSeederList.add(new Hotel(346864847, REIKARTZ, KIEV, roomsSeederList));
+        hotelsSeederList.add(new Hotel(945425451, REIKARTZ, ODESSA, roomsSeederList));
+        hotelsSeederList.add(new Hotel(345684884, REIKARTZ, LVOV, roomsSeederList));
+        hotelsSeederList.add(new Hotel(753456466, REIKARTZ, DONETSK, roomsSeederList));
+        hotelsSeederList.add(new Hotel(456468498, REIKARTZ, ZAPOROZHYE, roomsSeederList));
     }
 }
