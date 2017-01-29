94a4912c87b68174fc7250abd7672b6709d4618f
Enver <a.enver.com@gmail.com>
2017-01-29 18:25:08 +0200

    Logback_Add in pom.xml and Main.class

--- src/main/java/com/manardenza/Main.java
+++ src/main/java/com/manardenza/Main.java
@@ -1,6 +1,11 @@
 package com.manardenza;
 
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    static org.slf4j.Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
