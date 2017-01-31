9f90d32a5e9a69675f6305a627e8c5e1a44a8cb8
oONeVeNOo <koliushko.a@gmail.com>
2017-01-31 22:23:09 +0200

    Fix

--- src/main/java/com/manardenza/entity/Room.java
+++ src/main/java/com/manardenza/entity/Room.java
@@ -5,24 +5,13 @@ import lombok.Data;
 @Data
 public class Room extends AbstractObject {
 
-    private long id;
     private String name;
     private int person;
     private int price;
 
     public Room(String name, int person, int price) {
-        id = super.getId();
         this.name = name;
         this.person = person;
         this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Room{"
-                + "name='" + name + '\''
-                + ", person=" + person
-                + ", price=" + price
-                + '}';
     }
 }
