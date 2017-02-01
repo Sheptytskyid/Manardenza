5a2f13f277358a1923d8baacea2403da91d58a7d
Denys Sheptytskyi <neoxesis@mail.ru>
2017-02-01 13:22:06 +0200

    implemented logging of user actions and errors

--- src/main/java/com/manardenza/Main.java
+++ src/main/java/com/manardenza/Main.java
@@ -1,5 +1,7 @@
 package com.manardenza;
 
+import com.manardenza.utils.DatabaseSeeder;
+import com.manardenza.utils.Injector;
 import org.slf4j.LoggerFactory;
 
 public class Main {
@@ -7,6 +9,8 @@ public class Main {
     static org.slf4j.Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        DatabaseSeeder.seedDatabase();
+        Injector.getUserController().loginUser("Denys", "Sheptytskyi");
         System.out.println("Hello world!");
     }
 
