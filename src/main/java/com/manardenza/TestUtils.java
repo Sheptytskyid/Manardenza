bcd517cd1c8e2949c9415a41055b7b67cabbd68b
oONeVeNOo <koliushko.a@gmail.com>
2017-02-02 03:17:03 +0200

    Service layer tests is OK! Fixed

--- src/main/java/com/manardenza/TestUtils.java
+++ src/main/java/com/manardenza/TestUtils.java
@@ -17,23 +17,27 @@ public class TestUtils {
     public static final String CITY = "Kiev";
     public static final String FIRST_NAME = "Name";
     public static final String LAST_NAME = "Surname";
+    public static final String ROOM_NAME = "DOUBLE ROOM";
     public static final Date RESERVED_FROM = new Date(117, 1, 10);
     public static final Date RESERVED_TO = new Date(117, 1, 15);
     public static final User USER = new User(FIRST_NAME, LAST_NAME);
-    public static final Room ROOM = new Room("Room", 2, 300);
+    public static final Room ROOM = new Room("DOUBLE ROOM", 2, 300);
     public static final Hotel HOTEL = new Hotel(HOTEL_NAME, CITY, getRoomsSeedList());
     public static final Map<String, List<Room>> ROOMS_MAP = new HashMap<>();
     public static final List<Room> roomSeedTestList = getRoomsSeedList();
 
+    private TestUtils() {
+    }
+
     public static List<Room> getRoomsSeedList() {
         List<Room> testList = new ArrayList<>();
-        testList.add(new Room("DOUBLE ROOM", 2, 300));
-        testList.add(new Room("DOUBLE ROOM", 2, 300));
-        testList.add(new Room("DOUBLE ROOM", 2, 300));
-        testList.add(new Room("DOUBLE ROOM", 2, 300));
-        testList.add(new Room("DOUBLE ROOM", 2, 300));
-        testList.add(new Room("DOUBLE ROOM", 2, 300));
-        testList.add(new Room("DOUBLE ROOM", 2, 300));
+        testList.add(new Room(ROOM_NAME, 2, 300));
+        testList.add(new Room(ROOM_NAME, 2, 300));
+        testList.add(new Room(ROOM_NAME, 2, 300));
+        testList.add(new Room(ROOM_NAME, 2, 300));
+        testList.add(new Room(ROOM_NAME, 2, 300));
+        testList.add(new Room(ROOM_NAME, 2, 300));
+        testList.add(new Room(ROOM_NAME, 2, 300));
 
         return testList;
     }
@@ -66,8 +70,5 @@ public class TestUtils {
         Map<String, List<Room>> testRoomsMap = new HashMap<>();
         testRoomsMap.put(HOTEL_NAME, getRoomsListToReturn());
         return testRoomsMap;
-    }
-
-    private TestUtils() {
     }
 }
