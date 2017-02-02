23e3768fb9d100b01a76daa858fd97d42d7b5bc2
oONeVeNOo <koliushko.a@gmail.com>
2017-02-03 01:03:23 +0200

    test

--- src/test/java/com/manardenza/dao/AbstractDaoTest.java
+++ src/test/java/com/manardenza/dao/AbstractDaoTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -22,6 +23,7 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.powermock.api.mockito.PowerMockito.when;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
@@ -39,6 +41,7 @@ public class AbstractDaoTest {
     private FileInputStream inputStreamMock = PowerMockito.mock(FileInputStream.class);
     private FileOutputStream outputStreamMock = PowerMockito.mock(FileOutputStream.class);
     private List<Reservation> testReservationsList = new ArrayList<>();
+    private Reservation testReservation;
 
     @Before
     public void setUp() throws Exception {
@@ -46,38 +49,38 @@ public class AbstractDaoTest {
         whenNew(ObjectInputStream.class).withArguments(Matchers.any()).thenReturn(oisMock);
         whenNew(FileInputStream.class).withParameterTypes(String.class).withArguments(Matchers.any()).thenReturn(inputStreamMock);
         whenNew(FileOutputStream.class).withParameterTypes(String.class).withArguments(Matchers.any()).thenReturn(outputStreamMock);
-    }
-
-    @Test
-    public void save() throws Exception {
-        Reservation testReservation = new Reservation(TestUtils.RESERVED_FROM, TestUtils.RESERVED_TO,
-                TestUtils.USER, TestUtils.ROOM, TestUtils.HOTEL);
-        assertEquals(testReservation, reservationDao.save(testReservation));
-    }
-
-    @Test
-    public void delete() throws Exception {
-
-    }
-
-    @Test
-    public void saveAll() throws Exception {
-        ArrayList<Reservation> listToSaveReservations = new ArrayList<>();
-        when(reservationDao.database.addAll(testReservationsList)).thenReturn(true);
-        reservationDao.saveAll(testReservationsList);
-        assertEquals(true, reservationDao.database.addAll(testReservationsList));
-    }
-
-    @Test
-    public void getAll() throws Exception {
+        testReservation = new Reservation(TestUtils.RESERVED_FROM, TestUtils.RESERVED_TO,
+                new User(TestUtils.FIRST_NAME, TestUtils.LAST_NAME), TestUtils.ROOM, TestUtils.HOTEL);
         testReservationsList.add(new Reservation(TestUtils.RESERVED_FROM, TestUtils.RESERVED_TO,
                 new User(TestUtils.FIRST_NAME, TestUtils.LAST_NAME), TestUtils.ROOM, TestUtils.HOTEL));
         testReservationsList.add(new Reservation(TestUtils.RESERVED_FROM, TestUtils.RESERVED_TO,
                 new User(TestUtils.FIRST_NAME, TestUtils.LAST_NAME), TestUtils.ROOM, TestUtils.HOTEL));
         testReservationsList.add(new Reservation(TestUtils.RESERVED_FROM, TestUtils.RESERVED_TO,
                 new User("Test1", "User1"), TestUtils.ROOM, TestUtils.HOTEL));
+
+    }
+
+    @Test
+    public void save() throws Exception {
+        assertEquals(testReservation, reservationDao.save(testReservation));
+    }
+
+    @Test
+    public void delete() throws Exception {
+        testReservationsList.add(testReservation);
+        assertTrue(reservationDao.delete(testReservation));
+        Mockito.verify(oosMock, Mockito.times(1)).writeObject(testReservation);
+    }
+
+    @Test
+    public void saveAll() throws Exception {
+        assertTrue(reservationDao.saveAll(testReservationsList));
+        Mockito.verify(oosMock, Mockito.times(1)).writeObject(testReservation);
+    }
+
+    @Test
+    public void getAll() throws Exception {
         when(oisMock.readObject()).thenReturn(testReservationsList);
-        reservationDao.getAll();
         assertEquals(testReservationsList, reservationDao.getAll());
     }
 }
