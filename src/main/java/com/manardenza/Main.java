f67a47936ab4901397f980b938efa137825acc9f
oONeVeNOo <koliushko.a@gmail.com>
2017-02-03 23:02:42 +0200

    fix

--- src/main/java/com/manardenza/Main.java
+++ src/main/java/com/manardenza/Main.java
@@ -6,8 +6,8 @@ import org.slf4j.LoggerFactory;
 public class Main {
 
     static org.slf4j.Logger logger = LoggerFactory.getLogger(Main.class);
+    
     public static void main(String[] args) {
-        
         DatabaseSeeder.seedDatabase();
         System.out.println("Hello world!");
     }
